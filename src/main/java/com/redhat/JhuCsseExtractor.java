a06ea74cbf6e2018a0fc4555ee674872f263666a
Grant McCarthy <grantmc99@gmail.com>
2020-05-13 13:11:05 -0400

    Changed Camel route to dynamic (toD)

--- src/main/java/com/redhat/JhuCsseExtractor.java
+++ src/main/java/com/redhat/JhuCsseExtractor.java
@@ -3,6 +3,7 @@ package com.redhat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -14,137 +15,209 @@ import org.apache.camel.model.dataformat.BindyType;
 import org.apache.camel.model.dataformat.JsonLibrary;
 
 public class JhuCsseExtractor extends RouteBuilder {
-	  
-//    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM-dd-yyyy");  
-//    LocalDateTime now = LocalDateTime.now();
-//    LocalDateTime previous = LocalDateTime.now().minusDays(1);
-    
-//    String yesterday = dtf.format(previous);
-//    String today = dtf.format(now);
-	
-	
-    static String today = "05-07-2020";
-    static int yyyy = Integer.parseInt(today.substring(6,10));
-    static int mm = Integer.parseInt(today.substring(0,2));
-    static int dd = Integer.parseInt(today.substring(3,5));
 
 	
 	@Override
   public void configure() throws Exception {
 
 
+		from("timer:jhucsse?repeatCount=1")
+				.setHeader("nextFile", method(this, "computeNextFile"))
+				.loopDoWhile(method(this, "someMethodNameHere"))
+				.toD("${header.nextFile}")
+				.split().tokenize("\n", 1, true)
+				.unmarshal().bindy(BindyType.Csv, JhuCsseDailyReportCsvRecordv1.class)
+				.marshal().json(JsonLibrary.Jackson)
+				.to("kafka:jhucsse");
 
-    from("timer:jhucsse?repeatCount=1")
-    .toF("https:{{jhu.csse.baseUrl}}/%s.csv", today)
-    .split().tokenize("\n", 1, true)
-    .unmarshal().bindy(BindyType.Csv, JhuCsseDailyReportCsvRecord.class)
-    .marshal().json(JsonLibrary.Jackson)
-    .to("kafka:jhucsse");
-  }
-
-  @XmlRootElement(name="jhu-csse-report")
-  @CsvRecord(separator = ",", skipField=true, quote="\"")
-  public static class JhuCsseDailyReportCsvRecord {
-
-    @DataField(pos = 2)
-    private String admin2;
-	  
+	}
 	
-
-    @DataField(pos = 3)
-    private String provinceState;
-	  
-	  @DataField(pos = 4)
-	  private String countryRegion;
-	  
-	  @DataField(pos = 5)
-	  private String lastUpdate;
-	  
-	  @DataField(pos = 8)
-    private int confirmedCases;
-	  
-	  @DataField(pos = 9)
-    private int deaths;
-    
-//    @DataField(pos = 1)
-//    private String provinceState;
-//
-//    @DataField(pos = 2)
-//    private String countryRegion;
-//
-//    @DataField(pos = 3)
-//    private String lastUpdate;
-//
-//	@DataField(pos = 4)
-//	private int confirmedCases;
-//
-//    @DataField(pos = 5)
-//    private int deaths;
-    
-    private LocalDate reportDate;
-
-    public String getAdmin2() {
-      return admin2;
-    }
-
-    public void setAdmin2(String admin2) {
-      this.admin2 = admin2;
-    }
-
-    public String getProvinceState() {
-      return provinceState;
-    }
-
-    public void setProvinceState(String provinceState) {
-      this.provinceState = provinceState;
-    }
-
-    @XmlElement(name = "country")
-    public String getCountryRegion() {
-      return countryRegion;
-    }
-
-    public void setCountryRegion(String countryRegion) {
-      this.countryRegion = countryRegion;
-    }
-
-    public String getLastUpdate() {
-      return lastUpdate;
-    }
-
-    public void setLastUpdate(String lastUpdate) {
-      this.lastUpdate = lastUpdate;
-    }
-
-    public int getConfirmedCases() {
-      return confirmedCases;
-    }
-
-    public void setConfirmedCases(int confirmedCases) {
-      this.confirmedCases = confirmedCases;
-    }
-
-    public int getDeaths() {
-      return deaths;
-    }
-
-    public void setDeaths(int deaths) {
-      this.deaths = deaths;
-    }
-    
-    public Long getReportDate() {
-        Long epoch = 0L;
-    	try {
-        	 epoch = new SimpleDateFormat("MM-dd-yyyy").parse(today).getTime();
-        } catch (ParseException e) {
-        	e.printStackTrace();
-        }
-		return epoch;
+	public void computeNextFile() {
+		String startDateStr = "02-01-2020";
+		String endDateStr = "03-22-2020";
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM-dd-yyyy");
+		LocalDate date = LocalDate.parse(startDateStr, dtf);
+		LocalDate endDate = LocalDate.parse(endDateStr, dtf);
 	}
+  
+	@XmlRootElement(name="jhu-csse-report")
+	@CsvRecord(separator = ",", skipField=true, quote="\"")
+	static class JhuCsseDailyReportCsvRecordv2 {
 
-	public void setReportDate(LocalDate reportDate) {
-		this.reportDate = reportDate;
+
+
+		@DataField(pos = 2)
+		private String admin2;
+
+
+
+		@DataField(pos = 3)
+		private String provinceState;
+
+		@DataField(pos = 4)
+		private String countryRegion;
+
+		@DataField(pos = 5)
+		private String lastUpdate;
+
+		@DataField(pos = 8)
+		private int confirmedCases;
+
+		@DataField(pos = 9)
+		private int deaths;
+		
+		private Long reportDate;
+
+		public String getAdmin2() {
+			return admin2;
+		}
+
+		public void setAdmin2(String admin2) {
+			this.admin2 = admin2;
+		}
+
+		public String getProvinceState() {
+			return provinceState;
+		}
+
+		public void setProvinceState(String provinceState) {
+			this.provinceState = provinceState;
+		}
+
+		@XmlElement(name = "country")
+		public String getCountryRegion() {
+			return countryRegion;
+		}
+
+		public void setCountryRegion(String countryRegion) {
+			this.countryRegion = countryRegion;
+		}
+
+		public int getConfirmedCases() {
+			return confirmedCases;
+		}
+
+		public void setConfirmedCases(int confirmedCases) {
+			this.confirmedCases = confirmedCases;
+		}
+
+		public int getDeaths() {
+			return deaths;
+		}
+
+		public void setDeaths(int deaths) {
+			this.deaths = deaths;
+		}
+
+		public Long getReportDate() {
+			Long epoch = 0L;
+			try {
+				epoch = new SimpleDateFormat("MM-dd-yyyy").parse(getLastUpdate().substring(0,10)).getTime();
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			return epoch;
+		}
+
+		public String getLastUpdate() {
+			return lastUpdate;
+		}
+
+		public void setLastUpdate(String lastUpdate) {
+			this.lastUpdate = lastUpdate;
+		}
+
+		public void setReportDate(String lastUpdate) {
+			this.lastUpdate = lastUpdate;
+		}
 	}
+	
+	@XmlRootElement(name="jhu-csse-report")
+	@CsvRecord(separator = ",", skipField=true, quote="\"")
+	static class JhuCsseDailyReportCsvRecordv1 {
 
-  }
+		@DataField(pos = 1)
+		private String provinceState;
+
+		@DataField(pos = 2)
+		private String countryRegion;
+
+		@DataField(pos = 3)
+		private String lastUpdate;
+
+		@DataField(pos = 4)
+		private int confirmedCases;
+
+		@DataField(pos = 5)
+		private int deaths;
+		
+		private Long reportDate;
+
+
+		//    public String getAdmin2() {
+		//      return admin2;
+		//    }
+
+		//    public void setAdmin2(String admin2) {
+		//      this.admin2 = admin2;
+		//    }
+
+		public String getProvinceState() {
+			return provinceState;
+		}
+
+		public void setProvinceState(String provinceState) {
+			this.provinceState = provinceState;
+		}
+
+		@XmlElement(name = "country")
+		public String getCountryRegion() {
+			return countryRegion;
+		}
+
+		public void setCountryRegion(String countryRegion) {
+			this.countryRegion = countryRegion;
+		}
+
+		public void setReportDate(String lastUpdate) {
+			this.lastUpdate = lastUpdate;
+		}
+
+		public int getConfirmedCases() {
+			return confirmedCases;
+		}
+
+		public void setConfirmedCases(int confirmedCases) {
+			this.confirmedCases = confirmedCases;
+		}
+
+		public int getDeaths() {
+			return deaths;
+		}
+
+		public void setDeaths(int deaths) {
+			this.deaths = deaths;
+		}
+
+		public Long getReportDate() {
+			Long epoch = 0L;
+			try {
+				epoch = new SimpleDateFormat("MM-dd-yyyy").parse(getLastUpdate().substring(0,10)).getTime();
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			return epoch;
+		}
+
+		public String getLastUpdate() {
+			return lastUpdate;
+		}
+
+		public void setLastUpdate(String lastUpdate) {
+			this.lastUpdate = lastUpdate;
+		}
+
+
+	}
+  
 }
